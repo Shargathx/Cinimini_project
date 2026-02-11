aef6f7cf65fbc2d96f75698ed05eb14b96a8fdc8
Shargathx <martin.saarevali@hotmail.com>
2026-02-11 20:08:49 +0200

    Project setup

---
+++ Backend/src/main/java/com/Cinimini/projekt/CiniminiPilootprojektApplication.java
@@ -0,0 +1,13 @@
+package com.Cinimini.projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiniminiPilootprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiniminiPilootprojektApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/Cinimini/projekt/CiniminiPilootprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cinimini.projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiniminiPilootprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
