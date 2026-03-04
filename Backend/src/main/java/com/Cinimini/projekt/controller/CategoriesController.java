4a68446451514d9266cdd33b1efc1f3169d51780
piperal <piperal@tlu.ee>
2026-03-04 15:56:25 +0200

    added getallcategories for active categories

--- Backend/src/main/java/com/Cinimini/projekt/controller/CategoriesController.java
+++ Backend/src/main/java/com/Cinimini/projekt/controller/CategoriesController.java
@@ -1,4 +1,31 @@
 package com.Cinimini.projekt.controller;
 
+
+import com.Cinimini.projekt.entity.Category;
+import com.Cinimini.projekt.repository.CategoryInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class CategoriesController {
+
+    @Autowired
+    CategoryInterface categoryInterface;
+
+    @GetMapping("/categories/active")
+    private List<Category> findAll() {
+        List<Category> categories = categoryInterface.findAll();
+        List<Category> active_categories = new ArrayList<>(List.of());
+        for (Category category : categories) {
+            if(category.is_active()){
+                active_categories.add(category);
+            }
+        }
+        return active_categories;
+
+    }
 }
---
+++ Backend/src/main/java/com/Cinimini/projekt/repository/CategoryInterface.java
@@ -0,0 +1,8 @@
+package com.Cinimini.projekt.repository;
+
+
+import com.Cinimini.projekt.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryInterface extends JpaRepository<Category, Long> {
+}
