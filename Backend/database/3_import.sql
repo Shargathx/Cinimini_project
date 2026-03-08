da4b9c6e159fe7f8a6324d3cc1c357f08cc2ce39
Shargathx <martin.saarevali@hotmail.com>
2026-03-08 14:15:43 +0200

    SQL import script update

--- Backend/database/3_import.sql
+++ Backend/database/3_import.sql
@@ -10,10 +10,12 @@ VALUES (default, 'teacher');
 INSERT INTO public.category (id, name, description, is_active) VALUES (default, 'Heli', 'See mäng on heli baasil', true);
 INSERT INTO public.category (id, name, description, is_active) VALUES (default, 'Video', 'See mäng on video baasil', true);
 INSERT INTO public.category (id, name, description, is_active) VALUES (default, 'Pilt', 'See mäng on pildi baasil', true);
+INSERT INTO public.category (id, name, description, is_active) VALUES (default, 'Anomaalia', 'Mitteaktiivne kategooria, testimiseks', false);
 
 INSERT INTO public.game (id, name, category_id, description, is_active) VALUES (default, 'Mida näed pildilt?' , 1, 'Mida see pilt sind tundma paneb? Kas sinu emotsiooni mõjutab fakt, et pilt on värvitu/värviline?', true);
 INSERT INTO public.game (id, name, category_id, description, is_active) VALUES (default, 'Mida näed pildilt?' , 2, 'Mida see video sind tundma paneb? Kas sinu emotsiooni mõjutab fakt, et video on värvitu/värviline?', true);
 INSERT INTO public.game (id, name, category_id, description, is_active) VALUES (default, 'Mida näed pildilt?' , 3, 'Mida see heli sind tundma paneb? Kas sinu emotsiooni mõjutab fakt, et heli on efektiga/ilma?', true);
+INSERT INTO public.game (id, name, category_id, description, is_active) VALUES (default, 'Mida näed anomaalialt?' , 1, 'Mitteaktiivne mäng, testimiseks', false);
 
 
 INSERT INTO public.game_step (id, game_id, step_order) VALUES (default, 1, 1);
