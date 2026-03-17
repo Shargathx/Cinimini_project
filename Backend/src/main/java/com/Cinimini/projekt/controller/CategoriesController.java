341787c6a2633959020309c7524641fa6bf09abe
Shargathx <martin.saarevali@hotmail.com>
2026-03-17 20:50:51 +0200

    Changed activeCategories from private to public.

--- Backend/src/main/java/com/Cinimini/projekt/controller/CategoriesController.java
+++ Backend/src/main/java/com/Cinimini/projekt/controller/CategoriesController.java
@@ -3,11 +3,13 @@ package com.Cinimini.projekt.controller;
 import com.Cinimini.projekt.entity.Category;
 import com.Cinimini.projekt.service.CategoryService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @AllArgsConstructor
 @RestController
 public class CategoriesController {
@@ -15,7 +17,7 @@ public class CategoriesController {
     CategoryService categoryService;
 
     @GetMapping("categories")
-    private List<Category> activeCategories() {
+    public List<Category> activeCategories() {
         return categoryService.getAllActive();
     }
 
