a335796c5b7267022961de042d5330b19036bb17
A.Aruorg <aruorg@tlu.ee>
2026-03-26 00:40:52 +0200

    Added background, header style, footer adjustments, styled category page

--- Frontend/src/assets/pages/Categories.tsx
+++ Frontend/src/assets/pages/Categories.tsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react";
 import type { Category } from "../models/Category";
 import { Link } from "react-router-dom";
+import soundIcon from '../icons/sound.svg';
+import imageIcon from '../icons/picture.svg';
+import videoIcon from '../icons/video.svg';
+import './Categories.css';
+
+
 
 function Categories() {
     // renderdamine -> esmakordne komponendi pealetulek (nt uuele lehele minnes tehakse üldine lehe renderdus)
@@ -9,22 +15,41 @@ function Categories() {
     const [categories, setCategories] = useState<Category[]>([]);
     // uef -> enter
     useEffect(() => {
+        console.log("Backend URL:", import.meta.env.VITE_BACK_URL);
         fetch(import.meta.env.VITE_BACK_URL + "/categories")
             .then(res => res.json())
             .then(json => setCategories(json))
             .catch(err => console.error(err));
     }, []);
 
+        const borderColors = [
+        "rgba(124, 234, 255, 0.55)",
+        "rgba(220, 205, 244, 0.8)",
+        "rgba(145, 242, 190, 0.55)"
+        ];
+
     return (
-        <div>
-            {categories.map(category =>
-                <div key={category.id}>
-                    <Link to={`/categories/${category.id}`}>
-                        {category.id} - {category.name} - {category.description} - {category.active ? "Active" : "Inactive"}
+        <div className="categories-container">
+            {categories.map((category, index) => (
+                <div
+                    key={category.id}
+                    className="category-card"
+                    style={{ border: `10px solid ${borderColors[index % borderColors.length]}` }}
+                >
+                    <Link to={`/categories/${category.id}`} className="category-link">
+                        <div className="icon-wrapper">
+                            {category.name === "Heli" && <img src={soundIcon} alt="Heli" className="icon" />}
+                            {category.name === "Pilt" && <img src={imageIcon} alt="Pilt" className="icon" />}
+                            {category.name === "Video" && <img src={videoIcon} alt="Video" className="icon" />}
+                        <div className="category-info">
+                            {category.name} 
+                        </div>
+                        </div>
                     </Link>
-                </div>)}
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
 export default Categories
