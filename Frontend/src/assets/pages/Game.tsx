803a9c26b970caf9782838f4d4d8bfd7c8a63c3a
piperal <piperal@tlu.ee>
2026-06-04 10:09:24 +0300

    updates

--- Frontend/src/assets/pages/Game.tsx
+++ Frontend/src/assets/pages/Game.tsx
@@ -5,20 +5,43 @@ import { useEffect, useState } from 'react'
 
 function Game() {
 
+    //Praegu veel "barebones"
 
     const { id } = useParams()
     const { catid } = useParams()
-    const [gameSteps, setGameSteps] = useState({})
+
+    const [data, setData] = useState([])
+    const [questions, setQuestions] = useState([])
+    const [points, setPoints] = useState([])
 
     useEffect(() => {
         fetch(import.meta.env.VITE_BACK_URL + `/category/games/${id}/steps`)
             .then(res => res.json())
-            .then(json => setGameSteps(json))
+            .then(json => { setData(json); console.log(json) })
             .catch(err => console.error(err));
     }, [id])
-    return (<>
-    <div>{gameSteps.name}</div>
 
+    function getQuestions() {
+        setQuestions(data.gameSteps[0].questions)
+    }
+
+    function getPoints() {
+        setPoints(data.gameSteps[0].discussionPoints)
+    }
+
+
+    return (<>
+        <button onClick={() => { getQuestions() }}>Questions</button>
+        <div>Küsimused: </div>
+        {questions.map((question) => (
+            <div key={question.id}>{question.questionText}</div>
+        ))}
+
+        <button onClick={() => { getPoints() }}>Discussion points</button>
+        <div>Küsimused: </div>
+        {points.map((point) => (
+            <div key={point.id}>{point.discussionText}</div>
+        ))}
     </>
     )
 }
