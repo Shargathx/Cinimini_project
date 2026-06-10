629d00cab964f16b53501fd433289fed3cd3239b
A.Aruorg <aruorg@tlu.ee>
2026-06-10 15:32:09 +0300

    header popups

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,5 +1,8 @@
 import './Header.css';
 import  '../index.css';
+import ReactMarkdown from "react-markdown";
+import CloseBtn from "../assets/icons/closePopup.svg";
+import { useState } from 'react';
 // menüüriba (avalehe element (igal pool, viib tagasi avalehele), infonupp (kas headerisse või eraldi elemendina)
 import { useLocation } from 'react-router-dom';
 
@@ -7,12 +10,29 @@ function Header() {
     const location = useLocation();
     const path = location.pathname;
 
-    const showAddGame = ["/", "/categories"].includes(path);
-    const showHomePage = ["/categories", "/add-game", "/categories/1", "/categories/2", "/categories/3", "/categories/1/1", "/categories/2/1", "/categories/3/1"].includes(path);
-    const showCategories = ["/add-game", "/categories/1", "/categories/2", "/categories/3", "/categories/1/1", "/categories/1/1", "/categories/2/1", "/categories/3/1"].includes(path);
+    const showAddGame = ["/categories"].includes(path);
+    const showHomePage =
+        path === "/categories" ||
+        path === "/add-game" ||
+        path.startsWith("/categories/");
+    const showCategories = 
+        path === "/add-game" ||
+        path.startsWith("/categories/");
     const showPageInfo = ["/"].includes(path);
     const showAbout = ["/"].includes(path);
     const showCategoriesInfo = ["/categories"].includes(path);
+
+    const [text, setText] = useState("");
+    const [showPopup, setShowPopup] = useState(false);
+
+    const openPopup = async (file: string) => {
+        console.log("open popup clicked:", file);
+        const response = await fetch(file);
+        const data = await response.text();
+        
+        setText(data);
+        setShowPopup(true);
+    };
 
     return (
         <header className="header">
@@ -37,26 +57,43 @@ function Header() {
                 )}
 
                 {showPageInfo && (
-                    <button className="page-info-button">
+                    <button className="page-info-button" onClick={() => openPopup('/texts/page-info.txt')}>
                         MIS ON MINIKINO?
                     </button>
                 )}
 
                 {showAbout && (
-                    <button className="about-button">
+                    <button className="about-button" onClick={() => openPopup('/texts/about.txt')}>
                         MEIST
                     </button>
                 )}
 
                 {showCategoriesInfo && (
-                    <button className="categories-info-button">
+                    <button className="categories-info-button" onClick={() => openPopup('/texts/categories-info.txt')}>
                         TUTVU KATEGOORIATEGA
                     </button>
-                )}
+                )} 
+                </nav>
 
-            </nav>
+                 {showPopup && (
+                        <div className="popup-container">
+                            <button 
+                            className="popup-close-button" 
+                            onClick={() => { setShowPopup(false);
+                        }}
+                        >
+                        <img  src={CloseBtn} alt="Close" />
+                    </button>
+                    <div className="popup-text-container">
+                    <ReactMarkdown>{text}</ReactMarkdown>
+                    </div>
+                </div>
+                )}
+           
         </header>
+        
     );
+
 }
 
 export default Header;
