3bd8bc2d53055ed1c95c03b44d40cb4f7dfb22b4
A.Aruorg <aruorg@tlu.ee>
2026-06-15 12:14:32 +0300

    content centered, bug fixes and categories popup scrollbar.

--- Frontend/src/assets/pages/Home.tsx
+++ Frontend/src/assets/pages/Home.tsx
@@ -11,12 +11,14 @@ function Home() {
     }
 
     return (
-        <div className="game-start-container">
-            <img src={gameLogo} alt="Minikino logo" className="game-logo" />
-            <button id="gameStart" onClick={handleStart}>
-                <img src={gameStartIcon} alt="Alusta mängimist" />
-            </button>
-            <h1>Alusta Mängimist</h1>
+        <div className="home-page">
+            <div className="game-start-container">
+                <img src={gameLogo} alt="Minikino logo" className="game-logo" />
+                <button id="gameStart" onClick={handleStart}>
+                    <img src={gameStartIcon} alt="Alusta mängimist" />
+                </button>
+                <h1>Alusta Mängimist</h1>
+            </div>
         </div>
     )
 }
