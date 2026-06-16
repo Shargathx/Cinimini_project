ca6086ff0ea60b090614c2df45dd31cc3aab55b4
piperal <piperal@tlu.ee>
2026-06-16 08:28:54 +0300

    added register and login page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -8,11 +8,13 @@ import Home from './assets/pages/Home'
 import AddGame from './assets/pages/AddGame'
 import Game from './assets/pages/Game'
 import EditView from './components/edit-game/EditView'
+import Register from './components/login-components/Register'
+import Login from './components/login-components/Login'
 
 function App() {
   return (
     <BrowserRouter>
-    {/* stiil tuleks pärast eraldi .css faili teha */}
+      {/* stiil tuleks pärast eraldi .css faili teha */}
       <div style={{
         display: "flex",
         flexDirection: "column",
@@ -21,14 +23,16 @@ function App() {
         <Header />
 
         {/* stiil tuleks pärast eraldi .css faili teha */}
-        <main style={{ flex: 1, padding: "20px" }}> 
+        <main style={{ flex: 1, padding: "20px" }}>
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
             <Route path='/categories' element={<Categories />} />
             <Route path='/categories/:catid' element={<CategoryGame />} />
             <Route path='/categories/:catid/:id' element={<Game />} />
             <Route path='/add-game' element={<AddGame />} />
-            <Route path='/update-game/:id' element={<EditView/>}/>
+            <Route path='/update-game/:id' element={<EditView />} />
           </Routes>
         </main>
 
---
+++ Frontend/src/components/login-components/Register.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div>Register As User Here</div>
+  )
+}
+
+export default Register
