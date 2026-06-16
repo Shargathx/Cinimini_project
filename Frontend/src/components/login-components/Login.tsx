d0a92acd8ef5df566f9e129488d6430741e51687
piperal <piperal@tlu.ee>
2026-06-16 08:40:07 +0300

    added basic login component

--- Frontend/src/components/login-components/Login.tsx
+++ Frontend/src/components/login-components/Login.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    function createUsername(name: string) {
+        setUsername(name)
+    }
+
+    function createPassword(pass: string) {
+        setPassword(pass)
+    }
+
+    function handleSubmit() {
+        console.log(username)
+        console.log(password)
+    }
+
+    return (<>
+        <div>Log in here:</div><br />
+        <label>Kasutajanimi:</label><br></br>
+        <input onChange={(e) => { createUsername(e.target.value) }} autocomplete="off" id='username' name='username' type='text'></input><br /><br />
+        <label>Salasõna:</label><br></br>
+        <input onChange={(e) => { createPassword(e.target.value) }} id='password' name='password' type='password'></input><br /><br />
+        <button onClick={() => { handleSubmit() }}>Registreeri Kasutaja</button>
+    </>
+    )
 }
 
 export default Login
