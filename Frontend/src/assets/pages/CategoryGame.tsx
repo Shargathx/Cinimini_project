796ba3dce1a3c74ed89951581391cc376a79f6ed
piperal <piperal@tlu.ee>
2026-06-17 14:24:07 +0300

    added inactive games button

--- Frontend/src/assets/pages/CategoryGame.tsx
+++ Frontend/src/assets/pages/CategoryGame.tsx
@@ -1,90 +1,50 @@
 import { useParams, Link } from "react-router-dom";
-import { useFetch } from "../../components/hooks/useFetch"; // Double check this path matches your folder structure!
-import type { Game } from "../models/Game";
+import { useEffect, useState } from "react";
 import defaultGameIcon from '../icons/defaultGameIcon.svg';
 import './CategoryGame.css';
 
 function CategoryGame() {
     const { catid } = useParams<{ catid: string }>();
-    const { data: categoryGames, loading } = useFetch<Game[]>(`${import.meta.env.VITE_BACK_URL}/category/games/${catid}`, [catid]);
-    const { data: inactiveGames } = useFetch<Game[]>(`${import.meta.env.VITE_BACK_URL}/category/inactive-games`, [catid]);
-    if (loading) {
-        return (
-            <div className="game-loading-container" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
-                <p>Laeb mänge...</p>
-            </div>
-        );
-    }
+    const [activegames, setActivegames] = useState([])
+    const [inactivegames, setInactivegames] = useState([])
+    const [games, setGames] = useState([])
 
-    const gamesList = categoryGames ?? [];
-    const inactiveGamesList = inactiveGames ?? null
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_BACK_URL}/category/games/${catid}`, {
+            method: "GET"
+        }).then(res => res.json()).then(json => { setActivegames(json); setGames(json) })
+
+    }, [catid])
+
+
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_BACK_URL}/category/inactive-games`, {
+            method: "GET"
+        }).then(res => res.json()).then(json => setInactivegames(json))
+
+    }, [catid])
+
 
     function softDelete(gameId: number) {
-        fetch(import.meta.env.VITE_BACK_URL + `/games/${gameId}`, {
+        fetch(import.meta.env.VITE_BACK_URL + `/ games / ${gameId}`, {
             method: "DELETE"
         }).then(() => { window.location.reload() })
     }
 
     function hardDelete(gameId: number) {
-        fetch(import.meta.env.VITE_BACK_URL + `/games/delete-permanently/${gameId}`, {
+        fetch(import.meta.env.VITE_BACK_URL + `/ games / delete -permanently / ${gameId}`, {
             method: "DELETE"
         }).then(() => { window.location.reload() })
     }
 
-    return (
-        <div className="games-container">
-            <button onClick={() => {
-                {
-                    gamesList.map((game) => (
-                        <div className="game-item" key={game.id}>
-                            <Link
-                                to={`/categories/${catid}/${game.id}`}
-                                className={
-                                    "game-card" +
-                                    (catid === "1" ? " heli" : "") +
-                                    (catid === "2" ? " video" : "") +
-                                    (catid === "3" ? " pilt" : "")
-                                }
-                            >
-                                <img src={defaultGameIcon} alt="DefaultIcon" className="game-icon" />
 
-                                <div className="game-name">
-                                    {game.name}
-                                </div>
-                            </Link>
 
-                            <div className="gameButtons">
-                                <button
-                                    className="deleteBtn"
-                                    onClick={() => softDelete(Number(game.id))}
-                                >
-                                    SOFT DELETE
-                                </button>
-                                <button
-                                    className="deleteBtn"
-                                    onClick={() => hardDelete(Number(game.id))}
-                                >
-                                    HARD DELETE
-                                </button>
 
-                                <Link to={`/update-game/${game.id}`}>
-                                    <button
-                                        className="editBtn"
-                                        onClick={() => {
-                                            localStorage.setItem("mode", "edit");
-                                            localStorage.setItem("id", String(game.id));
-                                            localStorage.setItem("catid", String(catid));
-                                        }}
-                                    >
-                                        EDIT
-                                    </button>
-                                </Link>
-                            </div>
-                        </div>
-                    ))
-                }
-            }}></button>
-            {gamesList.map((game) => (
+    return (<>
+        <button onClick={() => { setGames(activegames) }}>Active games</button>
+        <button onClick={() => { setGames(inactivegames) }}>Inactive games</button>
+        <div id="gameContainer" className="games-container">
+            {games.map((game) => (
                 <div className="game-item" key={game.id}>
                     <Link
                         to={`/categories/${catid}/${game.id}`}
@@ -132,6 +92,7 @@ function CategoryGame() {
                 </div>
             ))}
         </div>
+    </>
     );
 }
 
