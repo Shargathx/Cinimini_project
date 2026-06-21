c19385501ae03216de3dbf212642fac99810a2bb
A.Aruorg <aruorg@tlu.ee>
2026-06-22 00:47:06 +0300

    liecense

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -12,9 +12,37 @@ function Footer() {
                         {name}
                     </div>
                 ))}
+                
             </div>
+            <div className='copyright-info'>
+             <span>This work is licensed under</span>
+
+                <a href="https://creativecommons.org/licenses/by-sa/4.0/">
+                    CC BY-SA 4.0
+                </a>
+
+                <img
+                    src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"
+                    alt="Creative Commons"
+                    style={{ maxWidth: "1em", maxHeight: "1em", marginLeft: ".2em" }}
+                />
+
+                <img
+                    src="https://mirrors.creativecommons.org/presskit/icons/by.svg"
+                    alt="Attribution"
+                    style={{ maxWidth: "1em", maxHeight: "1em", marginLeft: ".2em" }}
+                />
+
+                <img
+                    src="https://mirrors.creativecommons.org/presskit/icons/sa.svg"
+                    alt="Share Alike"
+                    style={{ maxWidth: "1em", maxHeight: "1em", marginLeft: ".2em" }}
+                />
+                </div>
+
 
             <div className="footer-right">
+               
                 <img src={logo} alt="Logo" />
             </div>
         </footer>
